cc5a6f07684db5686a5211dda5d49442d358a30a
Monika <manuela.monika97@gmail.com>
2020-03-31 14:56:13 +0800

    Implement /start welcome message & greetings

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const bot = new TelegramBot(token, { polling: true });
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
 
@@ -22,11 +18,17 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "<b>Welcome!</b>\nI'll remind you to order your meals every day when the form is open so you won't go hungry!", { parse_mode: 'HTML' });
+});
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  const greetings = ['hi', 'hello', 'hey'];
+  greetings.forEach((greeting) => {
+    if (msg.text.toString().toLowerCase().indexOf(greeting) === 0) {
+      bot.sendMessage(chatId, `Hello ${msg.from.first_name}!`);
+    }
+  });
 });
